a9c6b6749fe9d001a60139e1148c5dd0dac8f338
Karol Kowalski <ordon1313@gmail.com>
2017-01-23 20:04:07 +0100

    Add Curry Pizza

--- src/main/java/com/tobeannounced/pizza/solve.kt
+++ src/main/java/com/tobeannounced/pizza/solve.kt
@@ -1,8 +1,15 @@
 package com.tobeannounced.pizza
 
 fun solve(problem: PizzaProblem): Set<Set<Piece>> {
-    return possibleSlices(problem.pizza,
-            problem.pizza.pieces.first { !it.assigned },
-            problem.params.min,
-            problem.params.max)
+    return solve(problem.pizza, curryPizza(problem.params.min, problem.params.max))
+}
+
+private fun solve(pizza: Pizza, possibleSlices: (Pizza, Piece) -> Set<Set<Piece>>): Set<Set<Piece>> {
+    return possibleSlices(pizza, pizza.pieces.first { !it.assigned })
+}
+
+private fun curryPizza(minNumberOfEachKind: Int, maxSliceSize: Int): (Pizza, Piece) -> Set<Set<Piece>> {
+    return { pizza: Pizza, piece: Piece ->
+        possibleSlices(pizza, piece, minNumberOfEachKind, maxSliceSize)
+    }
 }
