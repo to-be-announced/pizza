b3d9ac52a4f8a8129dace8f2a15d413899c9f564
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-01-23 18:56:59 +0100

    Add data class

--- src/main/java/com/tobeannounced/pizza/Pizza.kt
+++ src/main/java/com/tobeannounced/pizza/Pizza.kt
@@ -1,4 +1,10 @@
 package com.tobeannounced.pizza
 
-class Pizza {
+class Pizza(val pieces: List<Piece>)
+
+class Piece(val r: Int, val c: Int, val kind: PieceKind, val assigend: Boolean)
+
+enum class PieceKind {
+    MUSHROOM,
+    TOMATO
 }
