2867181d3a353ef5c650e481da65e5e7e8825459
Karol Kowalski <ordon1313@gmail.com>
2017-01-23 19:01:04 +0100

    Add params to Pizza

--- src/main/java/com/tobeannounced/pizza/Pizza.kt
+++ src/main/java/com/tobeannounced/pizza/Pizza.kt
@@ -1,6 +1,6 @@
 package com.tobeannounced.pizza
 
-class Pizza(val pieces: List<Piece>)
+class Pizza(val pieces: List<Piece>, val width: Int, val height: Int, val min: Int, val max: Int)
 
 class Piece(val r: Int, val c: Int, val kind: PieceKind, val assigned: Boolean)
 
