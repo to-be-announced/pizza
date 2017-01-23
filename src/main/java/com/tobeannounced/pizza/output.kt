269de4463ef46ef82ccab6e7b297f6fe1fca2a28
Paweł Gajda <pawegio@gmail.com>
2017-01-23 20:14:40 +0100

    Output result for single slice

--- src/main/java/com/tobeannounced/pizza/output.kt
+++ src/main/java/com/tobeannounced/pizza/output.kt
@@ -2,4 +2,11 @@ package com.tobeannounced.pizza
 
 fun output(solution: Set<Set<Piece>>) = buildString {
     append(solution.count())
+    solution.forEach { slice ->
+        val rMin = slice.map { it.r }.min()
+        val rMax = slice.map { it.r }.max()
+        val cMin = slice.map { it.c }.min()
+        val cMax = slice.map { it.c }.max()
+        append("\n$rMin $cMin $rMax $cMax")
+    }
 }
--- src/test/java/com/tobeannounced/pizza/OutputTest.kt
+++ src/test/java/com/tobeannounced/pizza/OutputTest.kt
@@ -10,4 +10,13 @@ class OutputTest {
         val solution = emptySet<Set<Piece>>()
         assertEquals("0", output(solution))
     }
+
+    @Test
+    fun shouldOutputResultForSingleSlice() {
+        val solution = setOf(setOf(
+                Piece(0, 0, PieceKind.MUSHROOM, true),
+                Piece(0, 1, PieceKind.TOMATO, true)))
+        assertEquals("1\n" +
+                "0 0 0 1", output(solution))
+    }
 }
