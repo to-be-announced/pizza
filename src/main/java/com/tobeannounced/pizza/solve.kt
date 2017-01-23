e02f34c9c9b9f8c22992f3825ca83557cd864dc4
Karol Kowalski <ordon1313@gmail.com>
2017-01-23 20:34:11 +0100

    Update solution

--- src/main/java/com/tobeannounced/pizza/solve.kt
+++ src/main/java/com/tobeannounced/pizza/solve.kt
@@ -1,15 +1,46 @@
 package com.tobeannounced.pizza
 
 fun solve(problem: PizzaProblem): Set<Set<Piece>> {
-    return solve(problem.pizza, curryPizza(problem.params.min, problem.params.max))
+    return solve(problem.pizza, curryPizza(problem.params.min, problem.params.max)).first()
 }
 
-private fun solve(pizza: Pizza, possibleSlices: (Pizza, Piece) -> Set<Set<Piece>>): Set<Set<Piece>> {
-    return possibleSlices(pizza, pizza.pieces.first { !it.assigned })
+private fun solve(pizza: Pizza,
+                  possibleSlices: (Pizza, Piece) -> Set<Set<Piece>>,
+                  currentSolutions: List<Set<Set<Piece>>> = listOf(emptySet())): List<Set<Set<Piece>>> {
+    if (pizza.isSolved()) {
+        return currentSolutions
+    } else {
+        return solveUnsolved(pizza, possibleSlices, currentSolutions)
+    }
+}
+
+private fun solveUnsolved(pizza: Pizza, possibleSlices: (Pizza, Piece) -> Set<Set<Piece>>, currentSolutions: List<Set<Set<Piece>>>): List<Set<Set<Piece>>> {
+    val piece = pizza.pieces.first { !it.assigned }
+    val possibleSolutions = possibleSlices(pizza, piece).flatMap { it: Set<Piece> ->
+        solve(pizza.removeAll(it), possibleSlices, addToSolution(currentSolutions, it))
+    }
+    val alternativeSolution = solve(pizza.remove(piece), possibleSlices, currentSolutions)
+    return possibleSolutions + alternativeSolution
+}
+
+private fun addToSolution(currentSolutions: List<Set<Set<Piece>>>, newPiece: Set<Piece>): List<Set<Set<Piece>>> {
+    return currentSolutions.map { it.plusElement(newPiece) }
+}
+
+private fun Pizza.removeAll(pieces: Set<Piece>): Pizza {
+    return pieces.fold(this, Pizza::remove)
+}
+
+private fun Pizza.remove(piece: Piece): Pizza {
+    return copy(pieces - piece + piece.copy(assigned = true))
 }
 
 private fun curryPizza(minNumberOfEachKind: Int, maxSliceSize: Int): (Pizza, Piece) -> Set<Set<Piece>> {
     return { pizza: Pizza, piece: Piece ->
         possibleSlices(pizza, piece, minNumberOfEachKind, maxSliceSize)
     }
+}
+
+private fun Pizza.isSolved(): Boolean {
+    return pieces.all { it.assigned }
 }
