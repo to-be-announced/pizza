4add459699a1d839e6d144a1d22258db65c31495
Karol Kowalski <ordon1313@gmail.com>
2017-01-23 19:07:56 +0100

    Make Pizza data class

--- src/main/java/com/tobeannounced/pizza/Pizza.kt
+++ src/main/java/com/tobeannounced/pizza/Pizza.kt
@@ -1,8 +1,8 @@
 package com.tobeannounced.pizza
 
-class Pizza(val pieces: List<Piece>, val width: Int, val height: Int, val min: Int, val max: Int)
+data class Pizza(val pieces: List<Piece>, val width: Int, val height: Int, val min: Int, val max: Int)
 
-class Piece(val r: Int, val c: Int, val kind: PieceKind, val assigned: Boolean)
+data class Piece(val r: Int, val c: Int, val kind: PieceKind, val assigned: Boolean)
 
 enum class PieceKind {
     MUSHROOM,
